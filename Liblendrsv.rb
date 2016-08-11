e013f2407a1e161658207db6b81161814c65344a
hotuta <8985703@gmail.com>
2016-08-11 12:49:05 +0900

    first

---
+++ Liblendrsv.rb
@@ -0,0 +1,9 @@
+require_relative 'Liblendrsv/shibuya_Liblendrsv'
+require_relative 'Liblendrsv/minato_Liblendrsv'
+require_relative 'Liblendrsv/tokai_Liblendrsv'
+require_relative 'Liblendrsv/kawasaki_Liblendrsv'
+
+Shibuya.new.list
+Minato.new.list
+Tokai.new.list
+Kawasaki.new.list
