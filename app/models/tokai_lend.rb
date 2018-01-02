3dc3628c1d6f24add414c8437453783f97f6fb44
hotuta <hotuta@users.noreply.github.com>
2018-01-02 18:24:47 +0900

    DBにデータを入れるように変更

--- app/models/tokai_lend.rb
+++ app/models/tokai_lend.rb
@@ -5,18 +5,16 @@ class TokaiLend < ApplicationRecord
   # TODO: いつかCapybaraで書き直したい
   # include Capybara::DSL
 
-  def initialize
+  def self.list
     # Heroku用
-    # caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"binary" => "/app/.apt/usr/bin/google-chrome"})
-    # @wd = Selenium::WebDriver.for :chrome, desired_capabilities: caps
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"binary" => "/app/.apt/usr/bin/google-chrome"})
+    @wd = Selenium::WebDriver.for :chrome, desired_capabilities: caps
 
     # デバッグ用
-    @wd = Selenium::WebDriver.for :chrome
+    # @wd = Selenium::WebDriver.for :chrome
 
     @wd.manage.timeouts.implicit_wait = 30 # seconds
-  end
 
-  def list
     begin
       @wd.get "https://opac.time.u-tokai.ac.jp/webopac/login.do?url=ufisnd.do%3Fredirect_page_id%3D13"
       @wd.find_element(:name, "userid").click
@@ -33,17 +31,17 @@ class TokaiLend < ApplicationRecord
       @wd.get "https://library.time.u-tokai.ac.jp/?page_id=13&lang=japanese"
       @wd.get "https://library.time.u-tokai.ac.jp/index.php?action=v3search_view_main_libusesso&opacurl=https%3A%2F%2Fopac.time.u-tokai.ac.jp%2Fwebopac%2Flenlst.do"
 
-      tokailend = Icalendar::Calendar.new
-      tokailend.timezone do |t|
-        t.tzid = "Asia/Tokyo"
-        t.standard do |s|
-          s.tzoffsetfrom = "+0900"
-          s.tzoffsetto = "+0900"
-          s.dtstart = "19700101T000000"
-        end
-      end
-      tokailend.append_custom_property('X-WR-CALNAME', "東海大学貸出状況")
-      tokailend.append_custom_property('X-WR-CALDESC', "東海大学貸出状況カレンダー")
+      # tokailend = Icalendar::Calendar.new
+      # tokailend.timezone do |t|
+      #   t.tzid = "Asia/Tokyo"
+      #   t.standard do |s|
+      #     s.tzoffsetfrom = "+0900"
+      #     s.tzoffsetto = "+0900"
+      #     s.dtstart = "19700101T000000"
+      #   end
+      # end
+      # tokailend.append_custom_property('X-WR-CALNAME', "東海大学貸出状況")
+      # tokailend.append_custom_property('X-WR-CALDESC', "東海大学貸出状況カレンダー")
 
       takanawa = 0
       yoyogi = 0
@@ -57,6 +55,8 @@ class TokaiLend < ApplicationRecord
       end
 
       (2..101).each do |bn|
+        tokai_lend = TokaiLend.new
+
         contact = @wd.find_elements(:xpath, "/html/body/div/div/div/div[1]/form[1]/div/div[3]/table/tbody/tr[#{bn}]/td[4]")
 
         if contact.size == 0
@@ -70,49 +70,55 @@ class TokaiLend < ApplicationRecord
 
         doc.xpath("/html/body/div/div/div/div[1]/form[1]/div/div[3]/table/tbody/tr[#{bn}]/td[4]").each do |anchor|
           @location = anchor.content.delete("　").gsub(/(\s)/, "")
+          tokai_lend.location = @location
         end
-
         doc.xpath("/html/body/div/div/div/div[1]/form[1]/div/div[3]/table/tbody/tr[#{bn}]/td[9]/a").each do |anchor|
           @title = anchor.content.delete(" ").gsub(/(\s)/, "")
+          tokai_lend.title = @title
         end
-
-        @loc_title = "["+@location+"]"+@title
+        # tokai_lend.title = "[#{@location}]#{@title}"
         doc.xpath("/html/body/div/div/div/div[1]/form[1]/div/div[3]/table/tbody/tr[#{bn}]/td[5]/b").each do |anchor|
-          @date = anchor.content.delete("/").gsub(/(\s)/, "")
+          tokai_lend.date = anchor.content.delete("/").gsub(/(\s)/, "")
         end
 
         @wd.find_element(:xpath, "/html/body/div/div/div/div[1]/form[1]/div/div[3]/table/tbody/tr[#{bn}]/td[9]/a").click
 
         contact = @wd.find_elements(:xpath, "/html/body/div/div/div/div[1]/form[1]/div/div[2]/div[1]/table/tbody/tr[12]/td[contains(.,'KB')]")
 
-        @loc = @location
-
-        if contact.size != 0 && @loc == "高輪"
+        if contact.size != 0 && @location == "高輪"
           kb += 1
-        elsif contact.size == 0 && @loc == "高輪"
+        elsif contact.size == 0 && @location == "高輪"
           nomal += 1
         end
 
         if @location == "高輪"
           takanawa += 1
-          @location += takanawa.to_s
         elsif @location == "代々木"
           yoyogi += 1
-          @location += yoyogi.to_s
         end
 
+        # ISBN取得
+        @wd.find_element(:xpath, '//table[@class="opac_confirm_list"]//td/a').click
+        @wd.switch_to.window(@wd.window_handles.last)
+        tokai_lend.isbn = @wd.find_element(:xpath, '//*[@class="opac_syosi_list"]//table[contains(., "ISBN")]//td').text
+        @wd.close
+        @wd.switch_to.window(@wd.window_handles.last)
+
+        tokai_lend.save
+
+        # 一覧画面に遷移
         @wd.find_element(:xpath, "/html/body/div/div/div/div[1]/form[1]/div/div[1]/div/span[2]/a").click
 
-        tokailend.event do |ev|
-          ev.dtstart = Icalendar::Values::Date.new(@date)
-          ev.dtend = Icalendar::Values::Date.new(@date)
-          ev.summary = @loc_title.to_s
-        end
+        # tokailend.event do |ev|
+        #   ev.dtstart = Icalendar::Values::Date.new(@date)
+        #   ev.dtend = Icalendar::Values::Date.new(@date)
+        #   ev.summary = @loc_title.to_s
+        # end
       end
 
-      open(Rails.root.join('public/tokailend.ics'), "wb") do |ical|
-        ical.puts tokailend.to_ical
-      end
+      # open(Rails.root.join('public/tokailend.ics'), "wb") do |ical|
+      #   ical.puts tokailend.to_ical
+      # end
 
       puts "高輪#{takanawa}冊 代々木#{yoyogi}冊"
       puts "ノーマル#{nomal}冊 KB#{kb}冊 "
